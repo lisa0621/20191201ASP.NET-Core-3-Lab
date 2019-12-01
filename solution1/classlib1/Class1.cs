0483f23c9cd19f171dce534c1a8053172e2315e2
Sha-Pai LI <lisa0621@gmail.com>
2019-12-01 13:39:00 +0800

    Multi-targeting

--- solution1/classlib1/Class1.cs
+++ solution1/classlib1/Class1.cs
@@ -5,7 +5,12 @@ namespace classlib1
     public class Class1
     {
         public string Go(){
-            return "Go!!!!!!!!";
+            ////return "Go!!!!!!!!";
+            #if NET472
+            return "NET 472";
+            #else
+            return ".NET Standand 2.0";
+            #endif
         }
     }
 }
