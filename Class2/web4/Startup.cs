dc3a20eadff687d1ceadd01800f8455d46b77976
Sha-Pai LI <lisa0621@gmail.com>
2019-12-07 12:24:07 +0800

    web4 api

--- Class2/web4/Startup.cs
+++ Class2/web4/Startup.cs
@@ -31,17 +31,27 @@ namespace web4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //middleware Handle了 return 重來
+            //是不是開發環境
             if (env.IsDevelopment())
             {
+                //顯示例外頁面資訊
                 app.UseDeveloperExceptionPage();
             }
 
+            //不是Https導頁為Https，重來
             app.UseHttpsRedirection();
 
+            //是不是靜態檔實體路徑wwwroot，擺這沒有權限
+            ////app.UseStaticFiles();
+
+            //路由
             app.UseRouting();
 
+            //是否有權限
             app.UseAuthorization();
 
+            //一定擺在最後
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
