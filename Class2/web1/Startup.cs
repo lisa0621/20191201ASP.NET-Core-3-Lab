5f8fd2252eecffc866701f3f331b7c086f6ba671
Sha-Pai LI <lisa0621@gmail.com>
2019-12-07 10:46:18 +0800

    web1 404

--- Class2/web1/Startup.cs
+++ Class2/web1/Startup.cs
@@ -8,35 +8,31 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace web1
-{
-    public class Startup
-    {
+namespace web1 {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        public void ConfigureServices (IServiceCollection services) { }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseStatusCodePages((builder) => {
-                builder.Options
+            app.UseStatusCodePages (async context => {
+                context.HttpContext.Response.ContentType = "text/plain";
+
+                await context.HttpContext.Response.WriteAsync (
+                    "Status code page, status code: " +
+                    context.HttpContext.Response.StatusCode);
             });
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
+            app.UseEndpoints (endpoints => {
+                endpoints.MapGet ("/", async context => {
+                    await context.Response.WriteAsync ("Hello World!");
                 });
             });
         }
