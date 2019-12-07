a142baf42ee1edb3f23a2784e1aa34957b6958d6
Sha-Pai LI <lisa0621@gmail.com>
2019-12-07 16:38:06 +0800

    MVC

---
+++ Class2/web2/Models/Profile.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace web2.Models {
+    public class Profile {
+        public string Name { get; set; }
+        public string Tel { get; set; }
+    }
+}
--- Class2/web2/Startup.cs
+++ Class2/web2/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using web2.Models;
 
 namespace web2
 {
@@ -23,6 +24,9 @@ namespace web2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<Profile>(
+                Configuration.GetSection("Profile")
+            );
             services.AddControllersWithViews();
         }
 
