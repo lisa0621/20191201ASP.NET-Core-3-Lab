d521b3028925964fdc11675f9844231ad8555309
Sha-Pai LI <lisa0621@gmail.com>
2019-12-15 16:53:33 +0800

    Serilog & Seq

--- EFCoreDemo/EFCoreDemo/Controllers/WeatherForecastController.cs
+++ EFCoreDemo/EFCoreDemo/Controllers/WeatherForecastController.cs
@@ -33,7 +33,7 @@ namespace EFCoreDemo.Controllers
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
-            .ToArray();
+            .ToArray();           
         }
     }
 }
